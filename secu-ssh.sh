d045de98c0b154b9a89ed1b190166726d0fcd8ab
Logan <57043298+logan310@users.noreply.github.com>
2024-03-24 16:00:43 +0100

    Update secu-ssh.sh

--- secu-ssh.sh
+++ secu-ssh.sh
@@ -4,7 +4,7 @@
 # Version 6
 ---------------------------------------------------------------------------
 # Variables
-ipserv=$(hostname -I | cut -f1 -d' ') # Adresse IP de notre serveur web (tester avec %IP%)
+ipserv=$(hostname -I | cut -f1 -d' ') # Adresse IP de notre serveur
 mail=$(whiptail --inputbox "Pour recevoir des alertes par mail, saisissez une adresse e-mail" 8 39 exemple.test@gmail.com --title "Adresse mail" 3>&1 1>&2 2>&3)            # Mail utilisé pour recevoir les alertes
 portSSH=$(whiptail --inputbox "Pour changer le port SSH, merci de choisir un port entre 1024 a 65535" 8 39 63127 --title "Port SSH (22 par défault)" 3>&1 1>&2 2>&3)        # Utiliser le port SSH par defaut (22) n'est pas recomandé. il faut choisir un port entre 1024 a 65535.
 
